af65d324898957c13245e7e4baa8c4134481430c
safikh <safiuddin.khaja@gmail.com>
2022-10-16 19:35:05 +0530

    Add vim setup script

---
+++ vim-setup.sh
@@ -0,0 +1,6 @@
+# Install vim-plug for managing plugins
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Add Vim Config file
+cp ./.vimrc ~/.vimrc
